64338a39ddc72f1b4f1409d2193842e742490838
Gonçalo Silva <goncalossilva@gmail.com>
2010-08-11 01:18:35 +0100

    initial import of application (already requiring dummy-related gems in Gemfile)

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem 'ruby-prof', :git => 'git@github.com:wycats/ruby-prof.git'
+
+gem 'dummy_data'
+gem 'dummy_routes'
+gem 'dummy_performance_tests'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPerformanceCi::Application.config.secret_token = 'cd13311b8c73deccdffb048aa358cee6121df81ded3b632471cf605e2d9b2421b82529a5e89ec7557533a5801e3e12792fd4e04853199e4493863935575c4348'
