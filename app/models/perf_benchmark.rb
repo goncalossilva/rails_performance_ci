cebf1c255d289dfd3b82ef6e2c74fec0996bc951
Gonçalo Silva <goncalossilva@gmail.com>
2010-08-13 04:54:31 +0100

    handle the benchmarking history of each application

--- app/models/perf_benchmark.rb
+++ app/models/perf_benchmark.rb
@@ -2,6 +2,17 @@ class PerfBenchmark < ActiveRecord::Base
   belongs_to :app
   has_many :perf_tests, :dependent => :destroy
   
+  after_create :check_benchmark_history
+  
   validates :date, :presence => true
   validates :app, :presence => true
+  
+  private
+  
+  def check_benchmark_history
+    app = self.app
+    perf_benchmarks = app.perf_benchmarks
+    
+    perf_benchmarks.first.delete if perf_benchmarks.size > app.benchmark_history
+  end
 end
