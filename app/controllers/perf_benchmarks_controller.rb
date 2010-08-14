266d059a5180fce511907c9430585d0c176b8dd2
Gonçalo Silva <goncalossilva@gmail.com>
2010-08-14 02:46:07 +0100

    added benchmarks controller

---
+++ app/controllers/perf_benchmarks_controller.rb
@@ -0,0 +1,41 @@
+class PerfBenchmarksController < ApplicationController
+  before_filter :get_app
+  
+  # GET /perf_benchmarks
+  # GET /perf_benchmarks.xml
+  def index
+    @perf_benchmarks = @app.perf_benchmarks.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @perf_benchmarks }
+    end
+  end
+
+  # GET /perf_benchmarks/1
+  # GET /perf_benchmarks/1.xml
+  def show
+    @perf_benchmark = @app.perf_benchmarks.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @perf_benchmark }
+    end
+  end
+
+  # DELETE /perf_benchmarks/1
+  # DELETE /perf_benchmarks/1.xml
+  def destroy
+    @perf_benchmark = @app.perf_benchmarks.find(params[:id])
+    @perf_benchmark.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(app_perf_benchmarks_url) }
+      format.xml  { head :ok }
+    end
+  end
+  
+  def get_app
+    @app = App.find(params[:app_id])
+  end
+end
