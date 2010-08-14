b995cb7b347d977c63cb117168f62491df4d06a8
Gonçalo Silva <goncalossilva@gmail.com>
2010-08-14 02:40:44 +0100

    loading "dir/" by hand (otherwise fails in production)

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RailsPerformanceCi
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(lib) # TODO: remove this on Rails RC 2
+    #config.autoload_paths += %W(lib) # TODO: remove this on Rails RC 2
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
