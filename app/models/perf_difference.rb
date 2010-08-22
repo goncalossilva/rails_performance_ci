53a606d2b4475f5eddc99ef068dcdd7e742c6482
Gonçalo Silva <goncalossilva@gmail.com>
2010-08-22 01:49:34 +0100

    ensure all fields are present

--- app/models/perf_difference.rb
+++ app/models/perf_difference.rb
@@ -1,4 +1,11 @@
 class PerfDifference < ActiveRecord::Base
   belongs_to :prev_method, :class_name => "PerfMethod"
   belongs_to :curr_method, :class_name => "PerfMethod"
+  
+  validates :prev_commit, :presence => true
+  validates :curr_commit, :presence => true
+  validates :test_name, :presence => true
+  validates :prev_method, :presence => true
+  validates :curr_method, :presence => true
+  validates :difference, :presence => true
 end
