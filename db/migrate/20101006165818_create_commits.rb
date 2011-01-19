2ac85bece852d30eeb88464d1a69b8ab69864eba
Gonçalo Silva <goncalossilva@gmail.com>
2011-01-19 19:16:14 +0000

    renamed commit parameters

--- db/migrate/20101006165818_create_commits.rb
+++ db/migrate/20101006165818_create_commits.rb
@@ -1,10 +1,10 @@
 class CreateCommits < ActiveRecord::Migration
   def self.up
     create_table :commits do |t|
-      t.string    :hash,    :null => false, :length => 32
+      t.string    :sha1,    :null => false, :length => 40
       t.string    :message, :length => 255
       t.string    :author,  :length => 255
-      t.timestamp :date
+      t.timestamp :time
       
       t.references :application
 
