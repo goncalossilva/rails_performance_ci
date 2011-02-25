170d920ea35c4777bbdc5be868937b5922822a04
Gonçalo Silva <goncalossilva@gmail.com>
2011-02-25 21:20:06 +0000

    improved commit's tests, removed ApplicationTest since it's not currently being used

--- test/unit/commit_test.rb
+++ test/unit/commit_test.rb
@@ -3,9 +3,11 @@
 require 'test_helper'
 
 class CommitTest < ActiveSupport::TestCase
+  fixtures :applications, :commits
+  
   def setup
-    @c = commits(:sample_first_commit)
-    @c.checkout
+    @c = Commit.find_by_sha1("57f0a94fefc27d40c3d8")
+    @c.checkout    
   end
   
   def teardown
@@ -17,10 +19,10 @@ class CommitTest < ActiveSupport::TestCase
   end
   
   def test_should_import_repo_info
-    assert_equal @c.sha1, "57f0a94fefc27d40c3d8f2b7950cab109640a434"
-    assert_equal @c.message, "initial import"
-    assert_equal @c.author, "Gonçalo Silva <goncalossilva@gmail.com>"
-    assert_equal @c.time, Time.parse("2010-08-11 16:33:22 +0000")
+    assert_equal "57f0a94fefc27d40c3d8f2b7950cab109640a434", @c.sha1
+    assert_equal "initial import", @c.message
+    assert_equal "Gonçalo Silva <goncalossilva@gmail.com>", @c.author
+    assert_equal Time.parse("2010-08-11 16:33:22 +0000"), @c.time
   end
 end
 
