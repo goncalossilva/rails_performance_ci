c83e028e457ae1f351b76134aa0d3c8c641f0336
Gonçalo Silva <goncalossilva@gmail.com>
2011-02-25 21:17:29 +0000

    improved routing of payload

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ RailsPerformanceCi::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
-  match 'applications/:id/github/:token' => 'applications#github'
+  
+  match 'applications/:id/github/:token' => 'applications#github', :as => :applications_github, :via => :post
   
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
